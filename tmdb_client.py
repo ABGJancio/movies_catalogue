45dbb565eccba655cf80d6aed688ffc176f4a748
Jan Kondratiuk <abg.projekt@wp.pl>
2021-04-12 23:03:11 +0200

    Almost done

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,11 +1,42 @@
 import requests
+import random
 
 
-API_TOKEN = "75e1b1da77728ffda369868e2947a0da"
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWUxYjFkYTc3NzI4ZmZkYTM2OTg2OGUyOTQ3YTBkYSIsInN1YiI6IjYwNzA4MmIwY2QyMDQ2MDA1OGExZjhiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XINtPP3u1t_em2nuU0Hflpjd5lgJpU1L91KYJQIBeaw"
 
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+# def get_popular_movies():
+#     endpoint = "https://api.themoviedb.org/3/movie/popular"
+#     headers = {
+#         "Authorization": f"Bearer {API_TOKEN}"
+#     }
+#     response = requests.get(endpoint, headers=headers)
+#     return response.json()
+
+
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    random.shuffle(data["results"])
+    return data["results"][:how_many]
+
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
     }
@@ -13,11 +44,21 @@ def get_popular_movies():
     return response.json()
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
-    return data["results"][:how_many]
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
 
 
-def get_poster_url(poster_api_path, size="w342"):
-    base_url = "https://image.tmdb.org/t/p/"
-    return f"{base_url}{size}/{poster_api_path}"
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
